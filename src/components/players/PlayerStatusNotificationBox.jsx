93924ea00ec21ea1ca96eaa8549d4881e200a8f2
MuhammadTufail1 <muhammad.tufail@nayatel.com>
2024-04-09 01:42:47 +0500

    Poker changes

--- src/components/players/PlayerStatusNotificationBox.jsx
+++ src/components/players/PlayerStatusNotificationBox.jsx
@@ -1,27 +1,31 @@
 import React from 'react';
 import { CSSTransition } from 'react-transition-group';
 
-function PlayerStatusNotificationBox({index, isActive, content, endTransition}) {
-    return(
-        <CSSTransition 
-            in={isActive} 
-            timeout={{
-                appear: 0,
-                enter: 0,
-                exit: 1250,
-               }}
-            classNames="transitionable-actionBox" 
-            onEntered={() => {
-              setTimeout(() => {
-                endTransition(index)
-              }, 25)
-            }}
-        >
-            <div className="actionBox">
-            {`${content}`}
-            </div>
-        </CSSTransition>
-    )
+function PlayerStatusNotificationBox({
+  index,
+  isActive,
+  content,
+  endTransition,
+}) {
+  console.log(index, 'INDEXXXXX', isActive, content, endTransition);
+  return (
+    <CSSTransition
+      in={isActive}
+      timeout={{
+        appear: 0,
+        enter: 0,
+        exit: 1250,
+      }}
+      classNames='transitionable-actionBox'
+      onEntered={() => {
+        setTimeout(() => {
+          endTransition(index);
+        }, 25);
+      }}
+    >
+      <div className='actionBox'>{`${content}`}</div>
+    </CSSTransition>
+  );
 }
 
 export default PlayerStatusNotificationBox;
